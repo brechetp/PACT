9da33582bbb29f7541bc88d31cad1a00df4b4489
Michaël Mollard <michael.mollard@telecom-paristech>
2014-01-13 17:35:54 +0100

    fantome de la fonction de gestion du jeu

---
+++ src/logiqueDeJeux/BeloteCoinche.java
@@ -0,0 +1,13 @@
+package logiqueDeJeux;
+
+import machineEtat.StateMachine;
+
+public class BeloteCoinche 
+{
+	public String belote(int Point, JoueurDistant joueur, EtatDuJeu etatDuJeu, StateMachine state)
+	{
+		return null;
+		
+	}
+	
+}
