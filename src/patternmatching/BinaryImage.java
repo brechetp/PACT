7441b990f28d9941ca3c678bfd14f3bc83519dfe
brechet <brechet@enst.fr>
2014-01-18 13:50:54 +0100

    Début d'implémentation des classes

--- src/patternmatching/BinaryImage.java
+++ src/patternmatching/BinaryImage.java
@@ -1,6 +1,20 @@
 package patternmatching;
 
+import java.util.ArrayList;
+
 public class BinaryImage {
+	
+	private int[][] frame = null;
+	private ArrayList<int[][]> connectedComponents = null;
+	
+	public ArrayList<int[][]> conncetedComponents(){
+		// la matrice frame est faite de 0 et de 1
+		// on conserve une table de correspondane tab initialisée à tab[i] = i
+		// il nous faut une méthode qui s'applique à un pixel, renvoyant le min et le max de ses prédécesseurs
+		
+		
+		return connectedComponents;
+	}
 	
 	
 
