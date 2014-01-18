f5670f246cc1843c85c7446df43c9b91791693e1
Michaël Mollard <michael.mollard@telecom-paristech>
2014-01-18 01:46:45 +0100

    Mise en place de la premiere version de machine à états adaptée à la table

--- src/machineEtat/StateMachine.java
+++ src/machineEtat/StateMachine.java
@@ -2,82 +2,181 @@ package machineEtat;
 
 public class StateMachine 
 {
-	private enum State {STATE1, STATE2, STATE3};
+	private enum State {Distribution,
+						Annonce,AnnoncePasse1,AnnoncePasse2,AnnoncePasse3,AnnonceAFaire,
+						DebutTour,DebutTour1,DebutTour2,DebutTour3,DebutTour4,FinDebutTour, 
+						SecondTour,SecondTour1, SecondTour2, SecondTour3, SecondTour4, 
+						ResteDesTours, };
 	private State state;
 	
 	 public StateMachine() 
 	 {
-		    this.state = State.STATE1;
+		    this.state = State.Distribution;
 	 }
 	 
-	 public void event1() 
+	 public void eventCarte(CardEvent carte) 
 	 {
 		 switch(this.state) {
-		 case STATE1:
-			 //Action
-			 this.state = State.STATE2;
+/*********************** Distribution *******************/		 
+		 case Distribution:
+			 if (true/*pas toutes les cartes*/) 
+				 this.state = State.Annonce;
+			 else 
+				 //Action
+				 if (true /*carte valide*/)
+					 this.state = State.Distribution;
 			 break;
-		 case STATE2:
-			 //Action
-			 this.state = State.STATE1;
+			 
+/*********************** Premier Tour **********************/			 
+		 case DebutTour:
+			 if (true /*carte valide*/)
+				 this.state = State.DebutTour1;
 			 break;
-		 case STATE3:
-			 //Action
-			 this.state = State.STATE1;
+		 case DebutTour1:
+			 if (true /*carte valide*/)
+				 this.state = State.DebutTour2;
 			 break;
+		 case DebutTour2:
+			 if (true /*carte valide*/)
+				 this.state = State.DebutTour3;
+			 break;
+		 case DebutTour3:
+			 if (true /*carte valide*/)
+				 this.state = State.DebutTour4;
+			 break;
+		 case DebutTour4:
+			 if (true /*carte valide*/)
+				 this.state = State.FinDebutTour;
+			 break;
+			 
+/************************ Second Tour ************************/			 
+		 case SecondTour:
+			 if (true /*carte valide*/)
+				 this.state = State.SecondTour1;
+			 break;
+		 case SecondTour1:
+			 if (true /*carte valide*/)
+				 this.state = State.SecondTour2;
+			 break;
+		 case SecondTour2:
+			 if (true /*carte valide*/)
+				 this.state = State.SecondTour3;
+			 break;
+		 case SecondTour3:
+			 if (true /*carte valide*/)
+				 this.state = State.SecondTour4;
+			 break;
+			
+/********************* Reste Des Tours **********************/			 
+		 case ResteDesTours:
+			 if (true /*carte valide*/)
+				 this.state = State.ResteDesTours;	 
+			 break;
+			 
+			 
 		 default:
 		    	//Nothing
 			 break;
 		 }
 	 }
 		 
-	 public void event2() {
-		 switch(this.state) {
-		 case STATE1:
-			 //Action
-			 this.state = State.STATE3;
-			 break;
-		 case STATE2:
-			 //Action
-			 this.state = State.STATE3;
-			 break;
-		 case STATE3:
-			 //Action
-			 this.state = State.STATE2;
-			 break;
-		 }
-	 }
-	 
-	 public void event3() 
+	 public void eventGestePasser() 
 	 {
 		 switch(this.state) 
 		 {
-		 case STATE1:
-			 //Action
-			 this.state = State.STATE1;
+		 case Annonce:
+			 this.state = State.AnnoncePasse1;
 			 break;
-		 case STATE2:
-			 //Action
-			 this.state = State.STATE1;
+		 case AnnoncePasse1:
+			 this.state = State.AnnoncePasse2;
 			 break;
-		 case STATE3:
-			 //Action
-			 this.state = State.STATE1;
+		 case AnnoncePasse2:
+			 this.state = State.AnnoncePasse3;
 			 break;
+		 case AnnonceAFaire:
+				 this.state = State.AnnoncePasse1;
+			 break;
+		 case AnnoncePasse3:
+			 if (true /*si il y a un annonce de faite*/)
+				 //Action
+				 this.state = State.DebutTour;
+			 else /*annonce pas faite*/
+				 this.state = State.Distribution;
+			 break;
+			 
 		 default:
 			 //Nothing
 			 break;
 		 }
 	 }
-	 
-	 public State getState()
+
+	public void eventGeste(MouvementEvent Geste) 
 	 {
-		 return this.state;
+		 switch(this.state) 
+		 {
+		 case Annonce:
+			 if (true /*bon geste*/)
+				 //Action
+				 this.state = State.AnnonceAFaire;
+			 break;
+		 case AnnoncePasse1:
+			 if (true /*bon geste*/)
+				 //Action
+				 this.state = State.AnnonceAFaire;
+			 break;
+		 case AnnoncePasse2:
+			 if (true /*bon geste*/)
+				 //Action
+				 this.state = State.AnnonceAFaire;
+			 break;
+		 case AnnoncePasse3:
+			 if (true /*bon geste*/)
+				 //Action
+				 this.state = State.AnnonceAFaire;
+			 break;
+		 case AnnonceAFaire:
+			 if (true /*bon geste*/)
+				 //Action
+				 this.state = State.AnnoncePasse1;
+			 break;
+			 
+		 default:
+			 //Nothing
+			 break;
+		 }
 	 }
-	 
-	 public void setState(State state)
+
+	public void eventFinPli()
+	{
+		switch(this.state)
+		{
+			case DebutTour4:
+				//Action
+				this.state = State.SecondTour;
+				break;
+				
+			case SecondTour4:
+				//Action
+				this.state = State.ResteDesTours;
+				break;
+				
+			case ResteDesTours:
+				if(true /*partie terminée*/)
+					;//Action
+				else if(true /*manche terminée*/)
+					//Action
+					this.state = State.Distribution;
+				else this.state = State.ResteDesTours;
+				break;
+				
+			default:
+				break;
+			
+		}
+	}
+	
+	public void eventQuit() 
 	 {
-		 this.state=state;
-	 }
-	 
+		 System.exit(0);
+	 } 
 }
