d5ba134b80d075a5d3a2a4b3c06c0345ecaccbf0
edouardmifsud <edouardmifsud@gmail.com>
2014-01-19 18:49:15 +0100

    Oriente le programme en objet prépare l'impl du ps code

--- src/patternmatching/BinaryImage.java
+++ src/patternmatching/BinaryImage.java
@@ -4,18 +4,26 @@ import java.util.ArrayList;
 
 public class BinaryImage {
 	
-	private int[][] frame = null;
-	private ArrayList<int[][]> connectedComponents = null;
+	private int[][] frame = null; // image binaire 0=background 1=objet
+	private ArrayList<int[][]> connectedComponents = null; //table de correspondance
+	private Pixel[] taggedBinaryImage = null; // image initiale où les 1 sont remplacés par des pixels étiquetés
+	private int size;
 	
-	public ArrayList<int[][]> conncetedComponents(){
-		// la matrice frame est faite de 0 et de 1
+	public int[][] conncetedComponents(){
 		// on conserve une table de correspondane tab initialisée à tab[i] = i
-		// il nous faut une méthode qui s'applique à un pixel, renvoyant le min et le max de ses prédécesseurs
+		// algo de double passage
 		
+		for (int i=0; i<size; i++){
+			for (int j=0; j<size; j++){
+			 taggedBinaryImage[i][j] = new Pixel (i,j,this);
+			}
+		}
 		
-		return connectedComponents;
+		return taggedBinaryImage;
 	}
 	
-	
+	public int getFrameValue(int x, int y){
+		return frame[x][y];
+	}
 
 }
