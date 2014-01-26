977aee889bfa685062dbbabc619cf1a9334e47fc
Michaël Mollard <michael.mollard@telecom-paristech>
2014-01-26 02:23:45 +0100

    Upload des pseudocode corrigé et premiere version du code java

---
+++ adaboost/ClassiFinal.java
@@ -0,0 +1,31 @@
+import java.io.Serializable;
+
+
+public final class ClassiFinal implements Serializable
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8869425576675025163L;
+	
+	private double[] A;
+	private ClassifSouche[] H;
+	
+	public ClassiFinal(double[] a, ClassifSouche[] h) 
+	{
+		super();
+		A = a;
+		H = h;
+	}
+	
+	public int result(double[] X)
+	{
+		
+		double result = 0;
+		for(int k=0;k<X.length;k++)
+		{
+			result=result+A[k]*H[k].result(X);
+		}
+		return (int) Math.signum(result);
+	}
+}
