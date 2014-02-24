1575dc5136e7a7c26b0a1432a78cfe5a7a6ec4af
Benjamin BONTEMPS <benjamin.bontemps@telecom-paristech.fr>
2014-02-24 13:53:03 +0100

    Merge branch 'origin/LogiqueJeu'

---
+++ src/iug/IUGMain.java
@@ -0,0 +1,17 @@
+package iug;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class IUGMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JFrame window = new JFrame();
+		window.setSize(1000,500);
+		//window.setUndecorated(true);
+		window.setVisible(true);
+	}
+
+}
