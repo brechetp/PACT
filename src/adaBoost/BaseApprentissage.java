b66c539a6ed3f4e45178116a7f92b2eb75ee9db8
Benjamin BONTEMPS <benjamin.bontemps@telecom-paristech.fr>
2014-05-01 12:55:21 +0200

    rajout du début de passer vs accept

--- src/adaBoost/BaseApprentissage.java
+++ src/adaBoost/BaseApprentissage.java
@@ -2,7 +2,9 @@ package adaBoost;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
@@ -259,7 +261,187 @@ public class BaseApprentissage {
 		}
 		
 		mainada2 test = new mainada2();
-		return test.main(base,base_test);
+		ClassiFinal[] classi = test.main(base,base_test);
+		
+		/******************************** Classi passer vs accepter ************************************/
+		
+		try {
+			
+			double[][] basevs = new double[62+78][14];
+			for (int k = 5; k<5+6;k++)
+			{
+				ObjectInputStream ois =  new ObjectInputStream(new FileInputStream(tableau_fichiers.get(k))) ;
+				ListeDeMouvements hyk = (ListeDeMouvements)ois.readObject();
+			
+				int nb_exemples = hyk.size() ;
+				
+				for (int i = 0 ; i < nb_exemples ; i++)
+				{					
+					int nb_frames = hyk.get(i).getSize() ;
+					//System.out.println(nb_frames + " frames dans l'exemple " + i + " du fichier " + k);
+					
+					double[] descripteurs = new double[Classification.NOMBRE_DE_FEATURES] ;
+    			
+					for (int j = 5 ; j < nb_frames ; j++) //On enlève les 5 premières frames pour l'entrée du geste dans la zone
+					{
+						
+						descripteurs[0] =+ hyk.get(i).get(j).get(0) ; //nombre de mains
+						descripteurs[1] =+ hyk.get(i).get(j).get(7) ; //nombre de doigts de la main 1
+						
+						double X_normale1 = hyk.get(i).get(j).get(38) ; 
+						double Y_normale1 = hyk.get(i).get(j).get(39) ; 
+						double Z_normale1 = hyk.get(i).get(j).get(40) ; 
+						double Norme_normale1 = Math.sqrt(X_normale1*X_normale1+Y_normale1*Y_normale1+Z_normale1*Z_normale1) ;
+						if (descripteurs[0]>0) {
+							descripteurs[2] = +X_normale1 / Norme_normale1; //normale X de la main 1
+							descripteurs[3] = +Y_normale1 / Norme_normale1; //normale Y de la main 1
+							descripteurs[4] = +Z_normale1 / Norme_normale1; //normale Z de la main 1
+						
+						double X_translation1 = hyk.get(i).get(j).get(50) ;
+    					double Y_translation1 = hyk.get(i).get(j).get(51) ;
+    					double Z_translation1 = hyk.get(i).get(j).get(52) ;
+    					double Norme_translation1 = Math.sqrt(X_translation1*X_translation1+Y_translation1*Y_translation1+Z_translation1*Z_translation1) ;
+    					descripteurs[8] =+ X_translation1/Norme_translation1; //translation X de la main 1
+						descripteurs[9] =+ Y_translation1/Norme_translation1; //translation Y de la main 1
+						descripteurs[10] =+ Z_translation1/Norme_translation1; //translation Z de la main 1
+						
+						// On récupère la normale lors de la frame précédente pour calculer la rotation
+    					double X_normalePrecedente1 = hyk.get(i).get(j-1).get(38) ;
+    					double Y_normalePrecedente1 = hyk.get(i).get(j-1).get(39) ;
+    					double Z_normalePrecedente1 = hyk.get(i).get(j-1).get(40) ;
+    					double Norme_normalePrecedente1 = Math.sqrt(X_normalePrecedente1*X_normalePrecedente1+Y_normalePrecedente1*Y_normalePrecedente1+Z_normalePrecedente1*Z_normalePrecedente1) ;
+    					
+    					//descripteurs[14] =+ Math.acos((descripteurs[2]*X_normalePrecedente1 + descripteurs[3]*Y_normalePrecedente1 + descripteurs[4]*Z_normalePrecedente1)/Norme_normalePrecedente1 ) ; //rotation de la main 1
+						}
+    					/* Informations de la 2eme main */
+    					double X_normale2 = hyk.get(i).get(j).get(41) ; 
+						double Y_normale2 = hyk.get(i).get(j).get(42) ; 
+						double Z_normale2 = hyk.get(i).get(j).get(43) ; 
+						double Norme_normale2 = Math.sqrt(X_normale2*X_normale2+Y_normale2*Y_normale2+Z_normale2*Z_normale2) ;
+						if (descripteurs[0]>1) {
+							descripteurs[5] = +X_normale2 / Norme_normale2; //normale X de la main 2
+							descripteurs[6] = +Y_normale2 / Norme_normale2; //normale Y de la main 2
+							descripteurs[7] = +Z_normale2 / Norme_normale2; //normale Z de la main 2
+							double X_translation2 = hyk.get(i).get(j).get(53);
+							double Y_translation2 = hyk.get(i).get(j).get(54);
+							double Z_translation2 = hyk.get(i).get(j).get(55);
+							double Norme_translation2 = Math
+									.sqrt(X_translation2 * X_translation2
+											+ Y_translation2 * Y_translation2
+											+ Z_translation2 * Z_translation2);
+							descripteurs[11] = +X_translation2
+									/ Norme_translation2; //translation X de la main 2
+							descripteurs[12] = +Y_translation2
+									/ Norme_translation2; //translation Y de la main 2
+							descripteurs[13] = +Z_translation2
+									/ Norme_translation2; //translation Z de la main 2
+						}
+					}
+					
+					
+					for (int m = 0 ; m < Classification.NOMBRE_DE_FEATURES ; m++)
+					{
+						descripteurs[m] = descripteurs[m] / (double)(nb_frames-5) ;
+						basevs[compteur][m] = descripteurs[m] ;
+					}
+			
+					compteur++ ;
+    			}
+				ois.close() ;
+			}
+			
+			for(int k=17;k<23;k++)
+			{
+				ObjectInputStream ois =  new ObjectInputStream(new FileInputStream(tableau_fichiers.get(k))) ;
+				ListeDeMouvements hyk = (ListeDeMouvements)ois.readObject();
+			
+				int nb_exemples = hyk.size() ;
+				
+				for (int i = 0 ; i < nb_exemples ; i++)
+				{					
+					int nb_frames = hyk.get(i).getSize() ;
+					//System.out.println(nb_frames + " frames dans l'exemple " + i + " du fichier " + k);
+					
+					double[] descripteurs = new double[Classification.NOMBRE_DE_FEATURES] ;
+    			
+					for (int j = 5 ; j < nb_frames ; j++) //On enlève les 5 premières frames pour l'entrée du geste dans la zone
+					{
+						
+						descripteurs[0] =+ hyk.get(i).get(j).get(0) ; //nombre de mains
+						descripteurs[1] =+ hyk.get(i).get(j).get(7) ; //nombre de doigts de la main 1
+						
+						double X_normale1 = hyk.get(i).get(j).get(38) ; 
+						double Y_normale1 = hyk.get(i).get(j).get(39) ; 
+						double Z_normale1 = hyk.get(i).get(j).get(40) ; 
+						double Norme_normale1 = Math.sqrt(X_normale1*X_normale1+Y_normale1*Y_normale1+Z_normale1*Z_normale1) ;
+						if (descripteurs[0]>0) {
+							descripteurs[2] = +X_normale1 / Norme_normale1; //normale X de la main 1
+							descripteurs[3] = +Y_normale1 / Norme_normale1; //normale Y de la main 1
+							descripteurs[4] = +Z_normale1 / Norme_normale1; //normale Z de la main 1
+						
+						double X_translation1 = hyk.get(i).get(j).get(50) ;
+    					double Y_translation1 = hyk.get(i).get(j).get(51) ;
+    					double Z_translation1 = hyk.get(i).get(j).get(52) ;
+    					double Norme_translation1 = Math.sqrt(X_translation1*X_translation1+Y_translation1*Y_translation1+Z_translation1*Z_translation1) ;
+    					descripteurs[8] =+ X_translation1/Norme_translation1; //translation X de la main 1
+						descripteurs[9] =+ Y_translation1/Norme_translation1; //translation Y de la main 1
+						descripteurs[10] =+ Z_translation1/Norme_translation1; //translation Z de la main 1
+						
+						// On récupère la normale lors de la frame précédente pour calculer la rotation
+    					double X_normalePrecedente1 = hyk.get(i).get(j-1).get(38) ;
+    					double Y_normalePrecedente1 = hyk.get(i).get(j-1).get(39) ;
+    					double Z_normalePrecedente1 = hyk.get(i).get(j-1).get(40) ;
+    					double Norme_normalePrecedente1 = Math.sqrt(X_normalePrecedente1*X_normalePrecedente1+Y_normalePrecedente1*Y_normalePrecedente1+Z_normalePrecedente1*Z_normalePrecedente1) ;
+    					
+    					//descripteurs[14] =+ Math.acos((descripteurs[2]*X_normalePrecedente1 + descripteurs[3]*Y_normalePrecedente1 + descripteurs[4]*Z_normalePrecedente1)/Norme_normalePrecedente1 ) ; //rotation de la main 1
+						}
+    					/* Informations de la 2eme main */
+    					double X_normale2 = hyk.get(i).get(j).get(41) ; 
+						double Y_normale2 = hyk.get(i).get(j).get(42) ; 
+						double Z_normale2 = hyk.get(i).get(j).get(43) ; 
+						double Norme_normale2 = Math.sqrt(X_normale2*X_normale2+Y_normale2*Y_normale2+Z_normale2*Z_normale2) ;
+						if (descripteurs[0]>1) {
+							descripteurs[5] = +X_normale2 / Norme_normale2; //normale X de la main 2
+							descripteurs[6] = +Y_normale2 / Norme_normale2; //normale Y de la main 2
+							descripteurs[7] = +Z_normale2 / Norme_normale2; //normale Z de la main 2
+							double X_translation2 = hyk.get(i).get(j).get(53);
+							double Y_translation2 = hyk.get(i).get(j).get(54);
+							double Z_translation2 = hyk.get(i).get(j).get(55);
+							double Norme_translation2 = Math
+									.sqrt(X_translation2 * X_translation2
+											+ Y_translation2 * Y_translation2
+											+ Z_translation2 * Z_translation2);
+							descripteurs[11] = +X_translation2
+									/ Norme_translation2; //translation X de la main 2
+							descripteurs[12] = +Y_translation2
+									/ Norme_translation2; //translation Y de la main 2
+							descripteurs[13] = +Z_translation2
+									/ Norme_translation2; //translation Z de la main 2
+						}
+					}
+					
+					
+					for (int m = 0 ; m < Classification.NOMBRE_DE_FEATURES ; m++)
+					{
+						descripteurs[m] = descripteurs[m] / (double)(nb_frames-5) ;
+						basevs[compteur][m] = descripteurs[m] ;
+					}
+			
+					compteur++ ;
+    			}
+				ois.close() ;
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
